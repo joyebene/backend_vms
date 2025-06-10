1b4be1cfff29ef4f7d1b9ffd075882ba6af06ef1
Jaymax-cyber <jossycode0@gmail.com>
2025-06-10 17:57:56 +0100

    updt

--- config/db.js
+++ config/db.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect("mongodb+srv://joyebene:569569@vms.rgwrsfl.mongodb.net/?retryWrites=true&w=majority&appName=vms", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('MongoDB Connection Failed:', error.message);
