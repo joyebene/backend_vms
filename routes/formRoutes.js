4e78b0b3e980fbacae6fcc354fdd01af9603405d
Jaymax-cyber <jossycode0@gmail.com>
2025-06-16 18:34:02 +0100

    updt

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -1,15 +1,11 @@
 import express from 'express';
-import multer from 'multer';
 import { submitContractorForm, submitVisitorForm } from '../controllers/formController.js';
 
 const router = express.Router();
 
 
 
-const upload = multer({ storage });
-
-
 router.post('/visitor', submitVisitorForm);
-router.post('/contractor',  upload.array('documents'), submitContractorForm);
+router.post('/contractor',  submitContractorForm);
 
 export default router;
