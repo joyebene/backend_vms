ad0a1c365474f8753c965ddb930eaaa0ba7d2589
Jaymax-cyber <jossycode0@gmail.com>
2025-06-19 18:35:33 +0100

    updt

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cors from "cors";
 import formRoutes from './routes/formRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
-import documentRoutes from './routes/documentRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import connectDB from './config/db.js'
 
@@ -20,7 +19,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/forms', formRoutes);
 app.use('/api/admin', adminRoutes); 
-app.use('/api/documents', documentRoutes)
+
 
 
 app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
