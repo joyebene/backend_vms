a46f7bb3c8f79f537141dc57587ce7f9e9165a51
Jaymax-cyber <jossycode0@gmail.com>
2025-06-24 18:38:19 +0100

    updt

--- models/User.js
+++ models/User.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     password: { type: String, required: true },
-    confirmPassword: { type: String, required: true },
     phoneNumber: {type: String, required: true},
     role: { type: String, required: true },
 
