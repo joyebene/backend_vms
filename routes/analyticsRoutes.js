9fb63189b5bb7d51e136defe82503cc4c9d0e03c
Jaymax-cyber <jossycode0@gmail.com>
2025-06-25 02:15:42 +0100

    updt

--- routes/analyticsRoutes.js
+++ routes/analyticsRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { getVisitorStats, getAccessMetrics, getTrainingMetrics } from '../controllers/analyticsController.js';
-import { verifyToken } from '../middleware/authMiddleware.js';
+
 
 const router = express.Router();
 
-router.get('/visitors', verifyToken, getVisitorStats);
-router.get('/access', verifyToken, getAccessMetrics);
-router.get('/training', verifyToken, getTrainingMetrics);
+router.get('/visitors',  getVisitorStats);
+router.get('/access',  getAccessMetrics);
+router.get('/training',  getTrainingMetrics);
 
 export default router;
